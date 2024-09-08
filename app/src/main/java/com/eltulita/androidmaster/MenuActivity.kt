f3289ed97e7e3884a4f31a13503d2bfe28265529
Ublyudok-kun <pastitiaaan+1@gmail.com>
2024-09-08 01:12:58 -0300

    multiApp

---
+++ app/src/main/java/com/eltulita/androidmaster/MenuActivity.kt
@@ -0,0 +1,39 @@
+package com.eltulita.androidmaster
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.eltulita.androidmaster.firstapp.FirstAppActivity
+import com.eltulita.androidmaster.imccalculator.ImcCalculatorActivity
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnSaludApp = findViewById<AppCompatButton>(R.id.btnSaludApp)
+        val btnIMCApp = findViewById<AppCompatButton>(R.id.btnIMCApp)
+        btnSaludApp.setOnClickListener { navigateToSaludAPP() }
+        btnIMCApp.setOnClickListener { navigateToIMCApp() }
+    }
+
+    private fun navigateToSaludAPP(){
+        val intent = Intent(this, FirstAppActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToIMCApp(){
+        val intent = Intent(this, ImcCalculatorActivity::class.java)
+        startActivity(intent)
+    }
+}
